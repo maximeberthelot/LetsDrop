1d277aab40a0593bf055bad34bee9c8b945d1430
Simon Corompt <simon.corompt@hetic.net>
2015-02-22 17:23:37 +0100

    Added basic structure for the project. Linked Alamofire framework to project. Updated image assets folder.

--- LetsDrop/LoginViewController.swift
+++ LetsDrop/LoginViewController.swift
@@ -1,26 +1,27 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  LetsDrop
 //
-//  Created by Maxime Berthelot on 22/02/2015.
+//  Created by Simon Corompt on 22/02/2015.
 //  Copyright (c) 2015 Maxime Berthelot. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
