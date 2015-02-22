1f10c0b30804adb675a00d5fba46dd689b539789
maximeberthelot <hello@maximeberthelot.fr>
2015-02-22 23:08:43 +0100

    scrollView on Activity

--- LetsDrop/DroppedViewController.swift
+++ LetsDrop/DroppedViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DroppedViewController.swift
 //  LetsDrop
 //
 //  Created by Maxime Berthelot on 22/02/2015.
@@ -8,19 +8,17 @@
 
 import UIKit
 
-class ViewViewController: UIViewController {
+class DroppedViewController: UIViewController {
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
--- LetsDrop/MapViewController.swift
+++ LetsDrop/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  LetsDrop
 //
 //  Created by Maxime Berthelot on 22/02/2015.
@@ -7,20 +7,21 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
