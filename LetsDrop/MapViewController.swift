7865975b4638580ccae6a780f912260cdc1a178b
maximeberthelot <hello@maximeberthelot.fr>
2015-02-23 22:20:33 +0100

    Add MapKit and InitVIew

--- LetsDrop/MapViewController.swift
+++ LetsDrop/MapViewController.swift
@@ -2,7 +2,7 @@
 //  MapViewController.swift
 //  LetsDrop
 //
-//  Created by Maxime Berthelot on 22/02/2015.
+//  Created by Maxime Berthelot on 23/02/2015.
 //  Copyright (c) 2015 Maxime Berthelot. All rights reserved.
 //
 
@@ -10,13 +10,29 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let location = CLLocationCoordinate2D(
+            latitude: 51.50007773,
+            longitude: -0.1246402
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        mapView.addAnnotation(annotation)
     }
-    
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -24,4 +40,3 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     
 }
-
