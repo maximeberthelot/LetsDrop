5990b0e4b3e571f72c8e0eed6849cad0547c16e5
maximeberthelot <hello@maximeberthelot.fr>
2015-02-25 20:26:14 +0100

    Fix DroppedBtn & goTo CreateMessageView onclick

---
+++ LetsDrop/CreateMessageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreateMessageViewController.swift
+//  LetsDrop
+//
+//  Created by Maxime Berthelot on 25/02/2015.
+//  Copyright (c) 2015 Maxime Berthelot. All rights reserved.
+//
+
+import UIKit
+
+class CreateMessageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- LetsDrop/MapViewController.swift
+++ LetsDrop/MapViewController.swift
@@ -11,48 +11,48 @@ import MapKit
 import CoreLocation
 import AddressBookUI
 
+
+
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate,UISearchBarDelegate {
+    
     var request = MKLocalSearchRequest()
-    
-    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet var setButton: UIButton!
+    @IBOutlet weak var createMessageBtn: SpringButton!
     
     let manager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Set Dropped Btn ********************
+        var bounds: CGRect = UIScreen.mainScreen().bounds,
+        width:CGFloat = bounds.size.width,
+        height:CGFloat = bounds.size.height;
+        
+        println(height)
+          println(width)
+        createMessageBtn.center = (CGPointMake(self.view.center.x, self.view.center.y - createMessageBtn.frame.height+15))
+        
+        //
         searchBar.showsScopeBar = true
         searchBar.delegate = self
         
         if CLLocationManager.authorizationStatus() == .NotDetermined {
             manager.requestAlwaysAuthorization()
         }
-        
-        setButton.layer.cornerRadius = 5
-        
-     
-        let pinImage: UIImageView = UIImageView(image: UIImage(named:"pin1"))
-        pinImage.center = (CGPointMake(self.view.center.x, self.view.center.y - pinImage.frame.height))
-        self.mapView.delegate = self
-        self.mapView.addSubview(pinImage)
-        
-        
         if CLLocationManager.locationServicesEnabled() {
+            
             manager.delegate = self
             manager.desiredAccuracy = kCLLocationAccuracyBest
             manager.startMonitoringSignificantLocationChanges()
             println("started monitoring")
         }
-       
+        self.mapView.delegate = self
+        
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         var locValue:CLLocationCoordinate2D = manager.location.coordinate
-        println("locations = \(locValue.latitude) \(locValue.longitude)")
-        
-        
         var location = CLLocationCoordinate2D(
             latitude: locValue.latitude,
             longitude: locValue.longitude
@@ -62,50 +62,50 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         var region = MKCoordinateRegion(center: location, span: span)
         
         mapView.setRegion(region, animated: true)
-    
     }
-    
+ 
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
         println("Error while updating location " + error.localizedDescription)
     }
     
     
+    
+    
+    
     func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
+       var currentLocation = mapView.centerCoordinate
         
-        var currentLocation = mapView.centerCoordinate
         NSLog("Lon: \(currentLocation.longitude.description) Lat: \(currentLocation.latitude.description)")
-        //Convert lat & long
-        var longitude :CLLocationDegrees = Double(currentLocation.longitude)
-        var latitude :CLLocationDegrees = Double(currentLocation.latitude)
-        // push Lat & lon in var location
-        var location = CLLocation(latitude: latitude, longitude: longitude)
-        println(location)
         
-
+        //Convert lat & long
+        var longitude :CLLocationDegrees = Double(currentLocation.longitude),
+            latitude :CLLocationDegrees = Double(currentLocation.latitude),
+        // push Lat & lon in var location
+            location = CLLocation(latitude: latitude, longitude: longitude);
         CLGeocoder().reverseGeocodeLocation(location) {
+            
             (placemarks : [AnyObject]!, error : NSError!) in
             if placemarks != nil {
-                let p = placemarks[0] as CLPlacemark
+                let p = placemarks[0] as CLPlacemark     
                 let s = ABCreateStringWithAddressDictionary(p.addressDictionary, false)
                 println("you are at:\n\(s)") // do something with address
                 self.searchBar.text = String(s);
             }
         }
     }
-    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     //Search & SetMap with SearchBar
-    
     func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
         searchBar.resignFirstResponder()
         let s = searchBar.text
         if s == nil || countElements(s) < 5 { return }
-       
-        CLGeocoder().geocodeAddressString(s) {
+        let geo = CLGeocoder()
+        geo.geocodeAddressString(s) {
             (placemarks : [AnyObject]!, error : NSError!) in
             if nil == placemarks {
                 println(error.localizedDescription)
@@ -114,30 +114,44 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             self.mapView.showsUserLocation = false
             let p = placemarks[0] as CLPlacemark
             let mp = MKPlacemark(placemark:p)
+            self.mapView.setRegion(
+                MKCoordinateRegionMakeWithDistance(mp.coordinate, 1000, 1000),
+                animated: true)
         }
-
-        
-        
     }
+    
     
     //SetCenterMap
     
-    @IBAction func centerMap(sender: UIButton) {
-        println("myaciton");
-        var locValue:CLLocationCoordinate2D = manager.location.coordinate
-        println("locations = \(locValue.latitude) \(locValue.longitude)")
+    @IBAction func findMyLocate(sender: UIButton) {
         
-        
+        var userLocate:CLLocationCoordinate2D = manager.location.coordinate
         var location = CLLocationCoordinate2D(
-            latitude: locValue.latitude,
-            longitude: locValue.longitude
+            latitude: userLocate.latitude,
+            longitude: userLocate.longitude
         )
+        var bounds: CGRect = UIScreen.mainScreen().bounds,
+        width:CGFloat = bounds.size.width,
+        height:CGFloat = bounds.size.height;
         
-        var span = MKCoordinateSpanMake(0.5, 0.5)
-        var region = MKCoordinateRegion(center: location, span: span)
+        createMessageBtn.center = (CGPointMake(self.view.center.x, self.view.center.y - createMessageBtn.frame.height+15))
+        var mapCamera = MKMapCamera(lookingAtCenterCoordinate: location, fromEyeCoordinate: location, eyeAltitude: 1000)
         
-        mapView.setRegion(region, animated: true)
+        mapView.setCamera(mapCamera, animated: true)
+        
     }
-
     
+    
+    //Create a Message
+    
+    @IBAction func createMessage(sender: AnyObject) {
+       
+        
+        var createMessageStoryboard = UIStoryboard(name: "createmessage", bundle: nil)
+        var controller = createMessageStoryboard.instantiateViewControllerWithIdentifier("InitialCMessageViewController") as UIViewController
+        controller.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+    }
 }
+
