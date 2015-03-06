7d4ecfde75224d3902c9677598ec73e2c6c10338
maximeberthelot <hello@maximeberthelot.fr>
2015-03-06 02:40:17 +0100

    SetLifetime - Time change when button moved

--- LetsDrop/LifeTimeViewController.swift
+++ LetsDrop/LifeTimeViewController.swift
@@ -17,36 +17,49 @@ class LifeTimeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: "jumpToOtherTab:")
-        leftSwipeGesture.direction = UISwipeGestureRecognizerDirection.Left
+        leftSwipeGesture.direction = UISwipeGestureRecognizerDirection.Up
         view.addGestureRecognizer(leftSwipeGesture)
         
         var rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: "jumpToOtherTab:")
         rightSwipeGesture.direction = UISwipeGestureRecognizerDirection.Right
         view.addGestureRecognizer(rightSwipeGesture)
         
-        var panGesture = UIPanGestureRecognizer(target: self, action: "moveViewWithGestureRecognizer:")
+        var panGesture = UIPanGestureRecognizer(target: self, action: "moved:")
         dragBtn.addGestureRecognizer(panGesture)
         
         
-        var appDel:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-        var context:NSManagedObjectContext = appDel.managedObjectContext!
-        var request = NSFetchRequest(entityName: "Messages")
-        request.returnsObjectsAsFaults = false
-        var results:AnyObject = context.executeFetchRequest(request, error: nil)!
-        var lenght = results.count
+       // var appDel:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        //var context:NSManagedObjectContext = appDel.managedObjectContext!
+        //var request = NSFetchRequest(entityName: "Messages")
+        //request.returnsObjectsAsFaults = false
+        //var results:AnyObject = context.executeFetchRequest(request, error: nil)!
+        //var lenght = results.count
         
-        println(results[lenght-1])
+        //println(results[lenght-1])
      
 
     }
     
-    func moveViewWithGestureRecognizer(gestureRecognizer: UIPanGestureRecognizer) -> Void {
+    func moved(gestureRecognizer: UIPanGestureRecognizer) -> Void {
         self.view.bringSubviewToFront(lifeLabel)
         var location = gestureRecognizer.locationInView(self.view)
         dragBtn.center = location
         
-        println("coucou ")
+        let screenSize: CGRect = UIScreen.mainScreen().bounds,
+            screenHeight = screenSize.height,
+            currentBtnY = dragBtn.frame.origin.y,
+            myLifeTime = screenHeight/31,
+            getMyLifetime = Int((currentBtnY/myLifeTime)+1)
+        println("ok")
+        println(Int(getMyLifetime))
+        dragBtn.hidden = true
+        lifeLabel.text = String(getMyLifetime)
+        if gestureRecognizer.state == UIGestureRecognizerState.Ended{
+            println("ko")
+            dragBtn.hidden = false
+        }
         
+      
     }
     
     override func didReceiveMemoryWarning() {
