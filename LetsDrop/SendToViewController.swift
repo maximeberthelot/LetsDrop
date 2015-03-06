8c4bc571c7c23b717b15ed522d4ceccec31ea64a
Simon Corompt <simon.corompt@hetic.net>
2015-03-06 05:47:29 +0100

    Added sendTo view

---
+++ LetsDrop/SendToViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SendToViewController.swift
+//  LetsDrop
+//
+//  Created by Simon Corompt on 06/03/2015.
+//  Copyright (c) 2015 Maxime Berthelot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
